376709ba7c89835cf8c4aca2ed4a06e7dfd3f8af
Azidix <Azidix@gmail.com>
2025-05-27 19:44:11 +0200

    Update bot_inforoute2.py

--- bot_inforoute2.py
+++ bot_inforoute2.py
@@ -207,4 +207,9 @@ async def main():
     )
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    import nest_asyncio
+nest_asyncio.apply()
+loop = asyncio.get_event_loop()
+loop.create_task(main())
+loop.run_forever()
+
