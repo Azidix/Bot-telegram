8094d5b233a507c445892d74d66d94a8a52f8f44
Azidix <Azidix@gmail.com>
2025-05-27 18:39:02 +0200

    Update bot_inforoute2.py

--- bot_inforoute2.py
+++ bot_inforoute2.py
@@ -205,13 +205,11 @@ async def main():
     )
 
 if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except RuntimeError as e:
-        if "already running" in str(e):
-            loop = asyncio.get_event_loop()
-            loop.create_task(main())
-            loop.run_forever()
-        else:
-            raise
+    import nest_asyncio
+    import asyncio
 
+    nest_asyncio.apply()
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
+  
